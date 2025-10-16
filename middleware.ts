00eef8933273ebb764f00aa284a407b5d75e67c9
Alexys LAURENT <alexyslaurent.442@gmail.com>
2025-10-16 14:05:55 +0200

    Implements role-based route protection
    Adds middleware to protect routes based on user roles (admin, teacher, student).
    
    Redirects users to appropriate dashboards based on their account type.
    Prevents unauthorized access to specific routes.
    
    Adds authentication to login and register routes and redirects logged in users away from these routes.

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
-import { NextResponse } from "next/server";
-import { auth } from "./utils/auth";
+import { NextResponse } from 'next/server';
+import { auth } from './utils/auth';
 export default auth((req) => {
-  // req.auth
-		const regexUnprotectedRoutes = /^\/(?:api|login|register(?:\/.*)??)$/;
-	const loggedUser = req.auth
+	// req.auth
+	const regexUnprotectedRoutes = /^\/(?:api|login|register(?:\/.*)??)$/;
+	const loggedUser = req.auth;
 	const currentPath = req.nextUrl.pathname;
 
-	const response = NextResponse.next();	
+	const response = NextResponse.next();
 
 	// Protect all routes except unprotected ones
 	if (!loggedUser && !regexUnprotectedRoutes.test(currentPath)) {
@@ -18,9 +18,28 @@ export default auth((req) => {
 		return NextResponse.redirect(new URL('/', req.url));
 	}
 
+	// Redirect students to student dashboard if they try to access non-student routes but allow specific exam routes
+	if (loggedUser && loggedUser.user.accountType === 'student' && !currentPath.startsWith('/student')) {
+		if (!/^\/exams\/\d+\/students\/\d+$/.test(currentPath)) {
+			return NextResponse.redirect(new URL('/admin/dashboard', req.url));
+		}
+	}
+
+	// Redirect admin to admin dashboard if they try to access non-admin routes
+	if (loggedUser && loggedUser.user.accountType === 'admin' && !currentPath.startsWith('/admin')) {
+		return NextResponse.redirect(new URL('/admin/dashboard', req.url));
+	}
+
+	// Allow teachers to access only teacher routes and specific student exam routes
+	if (loggedUser && loggedUser.user.accountType === 'teacher' && !currentPath.startsWith('/teacher')) {
+		if (!/^\/exams\/\d+\/students\/\d+$/.test(currentPath)) {
+			return NextResponse.redirect(new URL('/admin/dashboard', req.url));
+		}
+	}
+
 	return response;
-})
+});
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-}
+	matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
