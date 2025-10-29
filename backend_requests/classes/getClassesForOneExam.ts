91a5b3c11bd6085cdff62b06ef58838ce82d9260
lou Wargnier <louisbw78@gmail.com>
2025-10-29 11:38:30 +0100

    [FIX] ma connerie

--- backend_requests/classes/getClassesForOneExam.ts
+++ backend_requests/classes/getClassesForOneExam.ts
@@ -2,10 +2,7 @@ import { Class } from '@/types/entitties';
 import { ApiError, SuccessResponse } from '@/types/requests';
 import { fetchApi } from '@/utils/fetchApi';
 
-export const getAllClassesForOneExam = async (idExam: number, limit?: number) => {
+export const getAllClassesForOneExam = async (idExam: number) => {
 	let url = `/api/classes/exams/${idExam}`;
-	if (limit) {
-		url += `?limit=${limit}`;
-	}
 	return (await fetchApi(url)) as ApiError | SuccessResponse<Class[]>;
 };
